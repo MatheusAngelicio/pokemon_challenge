3011d7348fde76e1e0064100c0a3ccfd050dac09
Matheus Angelicio <matheus.angelicio00@gmail.com>
2025-11-29 12:05:21 -0300

    style: add styles config

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -6,4 +6,8 @@ class AppColors {
 
   static const background = Color(0xFFF5F6FA);
   static const textPrimary = Color(0xFF1F1F1F);
+
+  static const white = Color(0xFFFFFFFF);
+  static const darkGray = Color(0x8A000000);
+  static const lightkGray = Color(0xFFE5E7EB);
 }
---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon_challenge/core/theme/app_colors.dart';
+
+class AppTextStyles {
+  static const small = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+    color: AppColors.textPrimary,
+  );
+
+  static const medium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: AppColors.textPrimary,
+  );
+
+  static const large = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static const xLarge = TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static const xxLarge = TextStyle(
+    fontSize: 40,
+    fontWeight: FontWeight.w600,
+    color: AppColors.textPrimary,
+  );
+
+  static const title = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.w500,
+    color: AppColors.textPrimary,
+  );
+}
