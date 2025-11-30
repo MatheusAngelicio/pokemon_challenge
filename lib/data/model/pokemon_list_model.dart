1b16250027c40c85183ccfc1940706e82dd14832
Matheus Angelicio <matheus.angelicio00@gmail.com>
2025-11-29 22:26:21 -0300

    refactor: improve nomenclature object

--- lib/data/model/pokemon_list_model.dart
+++ lib/data/model/pokemon_list_model.dart
@@ -3,14 +3,14 @@ import 'package:pokemon_challenge/domain/entity/pokemon_list_entity.dart';
 import 'pokemon_data_model.dart';
 
 final class PokemonListModel extends PokemonListEntity {
-  PokemonListModel({required List<PokemonDataModel> pokemonModel})
-    : super(pokemon: pokemonModel);
+  PokemonListModel({required List<PokemonDataModel> pokemonListModel})
+    : super(pokemonListEntity: pokemonListModel);
 
   factory PokemonListModel.fromMap(Map<String, dynamic> map) {
     final pokemonModel = (map['pokemon'] as List)
         .map((e) => PokemonDataModel.fromMap(e))
         .toList();
 
-    return PokemonListModel(pokemonModel: pokemonModel);
+    return PokemonListModel(pokemonListModel: pokemonModel);
   }
 }
--- lib/domain/entity/pokemon_list_entity.dart
+++ lib/domain/entity/pokemon_list_entity.dart
@@ -2,17 +2,18 @@ import 'package:flutter/foundation.dart';
 import 'pokemon_data_entity.dart';
 
 class PokemonListEntity {
-  final List<PokemonDataEntity> pokemon;
+  final List<PokemonDataEntity> pokemonListEntity;
 
-  PokemonListEntity({required this.pokemon});
+  PokemonListEntity({required this.pokemonListEntity});
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is PokemonListEntity && listEquals(other.pokemon, pokemon);
+    return other is PokemonListEntity &&
+        listEquals(other.pokemonListEntity, pokemonListEntity);
   }
 
   @override
-  int get hashCode => Object.hashAll(pokemon);
+  int get hashCode => Object.hashAll(pokemonListEntity);
 }
