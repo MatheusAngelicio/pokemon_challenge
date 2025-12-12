e6c9611821e0a21c87e5a63bbcf20e84df99b2b7
Matheus Angelicio <matheus.angelicio00@gmail.com>
2025-12-12 15:45:18 -0300

    fix(firebase): add firebase_core and firebase_analytics dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -7,11 +8,12 @@ import 'package:pokemon_challenge/presenter/cubits/pokemon_list/pokemon_list_cub
 import 'package:pokemon_challenge/presenter/cubits/pokemon_sort/pokemon_sort_cubit.dart';
 import 'package:pokemon_challenge/presenter/pages/pokemon_list/pokemon_list_page.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final getIt = GetIt.instance;
 
   PokemonInject.inject(getIt);
+  await Firebase.initializeApp();
 
   runApp(MyApp(getIt: getIt));
 }
