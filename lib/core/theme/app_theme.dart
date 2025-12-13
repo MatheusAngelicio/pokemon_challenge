7e6030e10fb302a4d3be5511f806ec2640122f99
Matheus Angelicio <matheus.angelicio00@gmail.com>
2025-12-12 22:40:16 -0300

    refactor(theme): add more default theme configuration

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon_challenge/core/theme/app_text_styles.dart';
 import 'app_colors.dart';
 
 class AppTheme {
   static ThemeData get light => ThemeData(
     colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
     scaffoldBackgroundColor: AppColors.background,
+    useMaterial3: true,
+    textTheme: TextTheme(bodyMedium: AppTextStyles.medium).apply(
+      bodyColor: AppColors.textPrimary,
+      displayColor: AppColors.textPrimary,
+    ),
   );
 }
